2ea83f0ccc0bb02be8dee61229dcf1e9db67dea2
bigtedde <big.tedde@gmail.com>
2023-07-18 17:15:07 -0700

    utils tests: test_normalize_path, test_get_path_datasize

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,13 @@
+import sys
 import uuid
 
 from vorta.keyring.abc import VortaKeyring
-from vorta.utils import find_best_unit_for_sizes, pretty_bytes
+from vorta.utils import (
+    find_best_unit_for_sizes,
+    get_path_datasize,
+    normalize_path,
+    pretty_bytes,
+)
 
 
 def test_keyring():
@@ -80,3 +86,45 @@ def test_pretty_bytes_metric_nonfixed2():
 def test_pretty_bytes_metric_large():
     s = pretty_bytes(10**30, metric=True, precision=1)
     assert s == "1000000.0 YB"
+
+
+def test_normalize_path():
+    # Test that path is normalized for macOS, but does nothing for other platforms.
+    input_path = '/Users/username/caf\u00e9/file.txt'
+    expected_output = '/Users/username/café/file.txt'
+
+    actual_output = normalize_path(input_path)
+
+    if sys.platform == 'darwin':
+        assert actual_output == expected_output
+    else:
+        assert actual_output == input_path
+
+
+def test_get_path_datasize(tmpdir):
+    # Create a temporary directory for testing
+    test_dir = tmpdir.mkdir("test_dir")
+    test_file = test_dir.join("test_file.txt")
+    test_file.write("Hello, World!")
+
+    # Create a subdirectory with a file to exclude
+    excluded_dir = test_dir.mkdir("excluded_dir")
+    excluded_file = excluded_dir.join("excluded_file.txt")
+    excluded_file.write("Excluded file, sound not be checked.")
+
+    exclude_patterns = [f"{excluded_dir}"]
+
+    # Test when the path is a directory
+    data_size, files_count = get_path_datasize(str(test_dir), exclude_patterns)
+    assert data_size == len("Hello, World!")
+    assert files_count == 1
+
+    # Test when the path is a file
+    data_size, files_count = get_path_datasize(str(test_file), exclude_patterns)
+    assert data_size == len("Hello, World!")
+    assert files_count == 1
+
+    # Test when the path is a directory with an excluded file
+    data_size, files_count = get_path_datasize(str(excluded_dir), exclude_patterns)
+    assert data_size == 0
+    assert files_count == 0
